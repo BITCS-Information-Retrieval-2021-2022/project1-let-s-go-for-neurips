a8db71b82e29c531ea393d51bc00165c849df3e0
BloodBlossom <1505115175@qq.com>
2021-11-26 08:27:37 +0800

    proxy_new

---
+++ proxy_new.py
@@ -0,0 +1,60 @@
+# 更新proxylist.txt中代理ip
+import random
+import requests
+from lxml import etree
+import os
+import time
+
+class getProxy:
+    def __init__(self):
+        self.url = 'https://www.kuaidaili.com/free/inha/'
+        self.headers = self.headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
+        self.path = 'proxylist_new.txt'
+        with open('proxylist_new.txt','r',encoding = 'UTF-8') as f:
+            self.proxies = f.readlines()
+        self.proxies = [c.strip() for c in self.proxies]
+        self.start_time = time.time()
+
+    def getHtml(self):
+        for i in range(76,4324):
+            time.sleep(random.random()*4+1)
+            # rnd = random.randint(1,4000)
+            # print('')
+            res = requests.get(self.url+str(i), proxies={'http':self.proxies[random.randint(0,1000)],'https':self.proxies[random.randint(0,1000)]},headers=self.headers)
+            res.encoding = 'utf-8'
+            html = res.text
+            self.parseHtml(html,i)
+
+    def parseHtml(self, html,index):
+        lst = []
+        # todo
+        parsehtml = etree.HTML(html)
+        iplist = parsehtml.xpath('//*[@id="list"]/table/tbody/tr/td[1]')
+        portlist = parsehtml.xpath('//*[@id="list"]/table/tbody/tr/td[2]')
+        iflist = parsehtml.xpath('//*[@id="list"]/table/tbody/tr/td[3]')
+        typelist = parsehtml.xpath('//*[@id="list"]/table/tbody/tr/td[4]')
+        addrlist = parsehtml.xpath('//*[@id="list"]/table/tbody/tr/td[5]')
+        num = 0
+        for x, y, z, m, n in zip(iplist, portlist, addrlist, iflist, typelist):
+            if x.text and y.text and z.text and m.text and n.text:
+                if 's' not in n.text and 'S' not in n.text:
+                    write_text = n.text + '://' + x.text + ':' + y.text + '\n'
+                    self.writeComment(write_text)
+                    num += 1
+        print('第',index,'页，共采集代理：',num,'总耗时：',time.time()-self.start_time)
+
+    def writeComment(self, lst):
+         with open(self.path, 'a') as f:
+             f.write(lst)
+
+    # 主函数
+    def main(self):
+        # if os.path.exists(self.path):
+        #     os.remove(self.path)
+        self.getHtml()
+
+
+if __name__ == '__main__':
+    proxy = getProxy()
+    proxy.main()
